0b680f3a48db3de553118fe474d7d7511639bb13
anish <anishpravitha@gmail.com>
2024-07-11 22:19:01 +0530

    completed

---
+++ routes/router.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+const express = require('express')
+const studentController = require('../controller/studentController')
+const multerConfig = require('../middlewares/multerMiddleware')
+const router = new express.Router()
+
+
+router.post('/importCSV',multerConfig.single('file'),studentController.importCSV)
+router.get('/getByAdmNo/:pid',studentController.getByAdmNo)
+router.put('/updateStudent/:pid',studentController.updateStudent)
+
+
+module.exports = router
